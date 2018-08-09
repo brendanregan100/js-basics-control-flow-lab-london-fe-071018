a6632250ae9188067f30864f0c31091c201588dd
Brendan regan <brendanregan100@gmail.com>
2018-08-09 19:19:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function scuberGreetingForFeet(distance){
     return ("This one is on me!");
   }
   else if (distance > 2000){
-    return ("We have exceeded free sample distance - you must pay now");
+    return ("We have exceeded free sample distance - you must pay now $30 fare");
   }
 }
