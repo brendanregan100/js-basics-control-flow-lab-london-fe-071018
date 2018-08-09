cc39311bbe67d8ea562b78aad2b7bc39d49d02b6
Brendan regan <brendanregan100@gmail.com>
2018-08-09 19:16:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 let distance = 400;
 
 function scuberGreetingForFeet(){
-if (distance <= 400);{
-  console.log("This one is on me!");
-}
-else if (distance > 400);{
-  console.log("We have exceeded free sample distance - you must pay now");
-}
+  if (distance <= 400){
+    return ("This one is on me!");
+  }
+  else if (distance > 400){
+    return ("We have exceeded free sample distance - you must pay now");
+  }
 }
