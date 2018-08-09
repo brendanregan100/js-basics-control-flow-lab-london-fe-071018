f6d8d062ea5911b955a17310b94e82d2beef4a3e
Brendan regan <brendanregan100@gmail.com>
2018-08-09 19:30:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,5 @@ function scuberGreetingForFeet(distance){
   else if (distance > 2500){
     return ("No can do.");
   }  
-    else if (distance > 2000){
-    return ("I will gladly take your thirty bucks.");
-  }
+  return ("I will gladly take your thirty bucks.");
 }
